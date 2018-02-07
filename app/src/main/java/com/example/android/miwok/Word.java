5ccdf0c4781ed70cbffddabe75ecdf43b01a95eb
aahdev <aGar@aahdev.com>
2018-02-07 15:13:34 +0100

    Images Working

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -2,14 +2,22 @@ package com.example.android.miwok;
 
 public class Word {
 
+    private static final int NO_IMAGE_PROVIDED = -1;
     /* Variables */
     private String mMiwokTranslation;
     private String mDefaultTranslation;
+    private int mImageSrc = NO_IMAGE_PROVIDED;
 
     /* New Word Object */
     public Word(String defaultTranslation, String miwokTranslation) {
         mMiwokTranslation = miwokTranslation;
         mDefaultTranslation = defaultTranslation;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageSrc) {
+        mMiwokTranslation = miwokTranslation;
+        mDefaultTranslation = defaultTranslation;
+        mImageSrc = imageSrc;
     }
 
     /* Getter */
@@ -19,6 +27,14 @@ public class Word {
 
     public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    public int getImageSrc() {
+        return mImageSrc;
+    }
+
+    public boolean hasImage() {
+        return mImageSrc != NO_IMAGE_PROVIDED;
     }
 
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -27,6 +28,16 @@ public class WordAdapter extends ArrayAdapter<Word> {
         }
 
         Word currentWord = getItem(position);
+
+        // Image
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image_view);
+
+        if (currentWord.hasImage()) {
+            imageView.setImageResource(currentWord.getImageSrc());
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            imageView.setVisibility(View.GONE);
+        }
 
         // Default Translation
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
